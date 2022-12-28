9eeccd98ee36579aa6a1045149c6a8d5999bfccb
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-12-28 15:16:18 -0500

    added functionality to the api nots post request

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -43,8 +43,53 @@ app.get('/api/notes', (req, res) => {
 // api post requestthat should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client with a unique id.
 //  can try to use fs.readfile to read db.json with notes, parse that data add the content to an array, strigify data, then use fs.writeFile to add the data (check data persistance activity starting at line 30)
 app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received to add a note`);
-    console.info(`${req.method} request received to add a note`);
-  });
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, body } = req.body;
+
+  // If all the required properties are present
+  if (title && body) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      body,
+      note_id: uuid(),
+    };
+
+    // Obtain existing notes
+    fs.readFile('.../db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notess back to the file
+        fs.writeFile(
+          '.../db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
   
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
